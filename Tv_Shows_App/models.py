2eddc80e4ca9e2b11cc67b4357d654cb11754ec4
Zahra Mughais <zahramughais@gmail.com>
2022-06-20 04:59:24 +0300

    adding validation

--- Tv_Shows_App/models.py
+++ Tv_Shows_App/models.py
@@ -1,5 +1,17 @@
+from xmlrpc.client import DateTime
 from django.db import models
-    
+from datetime import datetime
+class MovieManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData.get('title', [])) < 2:
+            errors['title'] = "Movie title should be at least 2 characters"
+        if len(postData.get('network', [])) < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        if len(postData.get('desc', [])) > 0 and len(postData.get('desc', [])) < 10:
+            errors['desc'] = "Description should be at least 10 characters"
+        return errors
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=45)
@@ -7,3 +19,5 @@ class Movie(models.Model):
     desc = models.TextField(default="no description")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = MovieManager()
--- Tv_Shows_App/views.py
+++ Tv_Shows_App/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render, HttpResponse,redirect
 from .models import *
+from django.contrib import messages
 
 def root(request):
     return redirect("/shows")
@@ -15,7 +16,12 @@ def create_new(request):
     return render(request, "create.html")
 
 def add_new(request):
-    if request.method == 'POST':
+    errors = Movie.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('./new')
+    else:
         newMovie = Movie.objects.create(
             title=request.POST['title'],
             network=request.POST['network'],
@@ -23,7 +29,7 @@ def add_new(request):
             desc=request.POST['desc'],
         )
         newMovie.save()
-    return redirect("Display", id = newMovie.id)
+        return redirect("Display", id = newMovie.id)
 
 def dis_show(request, id):
     context = {
@@ -38,14 +44,19 @@ def edit_show(request,id):
     return render(request, "edit.html", context)
 
 def update_show(request, id):
-    if request.method == 'POST':
+    errors = Movie.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('./edit')
+    else:
         update_movie = Movie.objects.get(id=id)
         update_movie.title= request.POST['title']
         update_movie.network= request.POST['network']
         update_movie.release_date= request.POST['rel_date']
         update_movie.desc= request.POST['desc']
         update_movie.save()
-    return redirect("Display", id = update_movie.id)
+        return redirect("Display", id = update_movie.id)
 
 def del_show(request, id):
     movie_to_del = Movie.objects.get(id=id)
